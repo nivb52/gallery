7e17580724263b7489982c5002ac8fd9c2d358d9
nivb52 <nivb52@gmail.com>
2019-08-15 20:16:31 +0300

    fix labels description and inccorect dates

--- js/projects-data.js
+++ js/projects-data.js
@@ -2,6 +2,7 @@
 
 function createProjects() {
     // ID IS ALSO IMG NAME AS JPG 
+    // TIME STAMP CONVERTOR :  https://www.epochconverter.com/
     return [
         //1 
         {
@@ -12,7 +13,7 @@ function createProjects() {
             url: 'https://yachtaway.herokuapp.com/',
             IsLocal: false,
 
-            publishedAt: 1564568859000,  // 31 July 2019 10:27:39
+            publishedAt: 1564568859000,  // 31 July 2019
             labels: ['VueX', 'VueJs', 'Vue Cli', 'Framework', 'sprint-3 weeks'],
             imgFromOtherSource: 'https://res.cloudinary.com/nivb/image/upload/w_400,h_210/hero/motor_yachts_lil9cp.jpg'
         },
@@ -25,7 +26,7 @@ function createProjects() {
             url: 'https://haifa-vacation-apartments.com/',
             isLocal: false,
 
-            publishedAt: 1564568859000,  // 31 July 2019 10:27:39
+            publishedAt: 1362157865000,  // 1 March 2013 
             labels: ['Vanilla JS', 'CSS', 'HTML', 'BOOTSTRAP 3'],
             imgFromOtherSource: false
         },
@@ -35,11 +36,11 @@ function createProjects() {
             id: 'haifavacation',
             name: 'Rent an Apartment',
             subtitle: 'Rent apartment in Israel, using CSS , HTML and JS, website',
-            desc: 'JavaScript website with translation, SEO in mind, ',
+            desc: 'Javascript website with translation, SEO in mind, rendering data with JavaScript',
             url: 'https://haifavacation.co.il/',
             IsLocal: false,
 
-            publishedAt: 1564568859000,  // 31 July 2019 10:27:39
+            publishedAt: 1452013865000,  // 1 May 2016
             labels: ['Vanilla JS', 'CSS', 'JavaScript', 'HTML'],
             imgFromOtherSource: false
         },
@@ -53,7 +54,7 @@ function createProjects() {
             url: 'projs/ball-board',
             IsLocal: true,
 
-            publishedAt: 1557829659000, // 14/5/19
+            publishedAt: 1557829659000, // 14 May 19
             labels: ['Game', 'Vanilla JS'],
             imgFromOtherSource: false
         },
@@ -62,12 +63,12 @@ function createProjects() {
         {
             id: 'canvas-meme-generator',
             name: 'Meme Generator',
-            subtitle: 'with VueX',
-            desc: 'des',
+            subtitle: 'with Cnavas',
+            desc: 'canvas meme generator. Drag, Edit and play it',
             url: 'https://nivb52.github.io/meme-generator/',
             IsLocal: false,
 
-            publishedAt: 1560421659000,  // 13.6.2019
+            publishedAt: 1560421659000,  // 13 Jun 2019
             labels: ['Canvas', 'git', 'sprint'],
             imgFromOtherSource: false
         },
@@ -83,7 +84,7 @@ function createProjects() {
             IsLocal: true,
 
             publishedAt: 1348693940000,
-            labels: ['Vanilla JS', 'matrix', '2D'],
+            labels: ['Game', 'Vanilla JS', 'matrix', '2D'],
             imgFromOtherSource: false
         },
 
@@ -96,7 +97,7 @@ function createProjects() {
             url: 'projs/book-shop',
             IsLocal: true,
 
-            publishedAt: 1559557659000, //  3.6.2019
+            publishedAt: 1559557659000, //  3 Jun 2019
             labels: ['jQuery', 'i18n'],
             imgFromOtherSource: false
         },
@@ -110,7 +111,7 @@ function createProjects() {
             url: 'projs/todo-vuex-localstorage',
             IsLocal: true,
 
-            publishedAt: 1564568859000, // 31.7.2019
+            publishedAt: 1564568859000, // 31 Jul 2019
             labels: ['VueX', 'VueJs', 'Vue Cli', 'Framework', 'sprint'],
             imgFromOtherSource: false
         },
@@ -123,7 +124,7 @@ function createProjects() {
             desc: 'Games That Matter. Touch Press provides high-quality games that are also opportunities to engage with key academic subjects and skills',
             url: 'projs/touch-in-nums',
             IsLocal: true,
-            publishedAt: 1557656859000, // 12.5.2019
+            publishedAt: 1557656859000, // 12 May 2019
             labels: ['Game', 'Vanilla JS', 'JavaScript'],
             imgFromOtherSource: false
         },
@@ -136,7 +137,7 @@ function createProjects() {
             url: 'https://mth.org.il',
             IsLocal: false,
 
-            publishedAt: 1557656859000, // 12.5.2019
+            publishedAt: 1488647465000, // 04 March 2017
             labels: ['Wordpress'],
             imgFromOtherSource: 'https://mth.org.il/assets/media/2019/06/IMG_20171211_081142-1024x578.jpg'
         },
